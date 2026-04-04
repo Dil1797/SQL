04b5f1ebf4b3ac79a577675724faca7c970f9268
Dil1797 <147470344+Dil1797@users.noreply.github.com>
2026-04-03 23:59:29 -0400

    Add files via upload

---
+++ Limit_Aliasing.sql
@@ -0,0 +1,39 @@
+-- Limit & Aliasing
+SELECT *
+FROM employee_demographics
+LIMIT 3;
+
+SELECT *
+FROM employee_demographics
+ORDER BY age DESC
+LIMIT 3;
+
+SELECT *
+FROM employee_demographics
+ORDER BY age DESC
+LIMIT 3, 1;
+
+SELECT *
+FROM employee_demographics
+ORDER BY age DESC
+LIMIT 2, 1;
+
+-- Aliasing
+SELECT gender, AVG(age)
+FROM employee_demographics
+GROUP BY gender;
+
+SELECT gender, AVG(age) 
+FROM employee_demographics
+GROUP BY gender
+HAVING AVG(age) > 40;
+
+SELECT gender, AVG(age) AS avg_age
+FROM employee_demographics
+GROUP BY gender
+HAVING avg_age > 40;
+
+SELECT gender, AVG(age) avg_age
+FROM employee_demographics
+GROUP BY gender
+HAVING avg_age > 40;
